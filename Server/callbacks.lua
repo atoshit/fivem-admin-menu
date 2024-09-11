8b3cf42dd80afefd6987db632eb9b524772bb0a8
atoshic17 <95099733+atoshit@users.noreply.github.com>
2024-09-11 10:52:57 +0200

    Add files via upload

---
+++ Server/callbacks.lua
@@ -0,0 +1,51 @@
+--[[
+Copyright © 2024 Atoshi
+
+All rights reserved.
+
+This FiveM base, "Outlaws" and all of its associated files, code, and resources are protected by copyright law. Unauthorized reproduction, distribution, or modification of this base, in whole or in part, without the express permission of the copyright holder, is strictly prohibited.
+
+For licensing inquiries or permission requests, please contact:
+
+https://discord.gg/fivedev
+
+Thank you for respecting our intellectual property rights.
+]]
+
+ESX.RegisterServerCallback('admin:getPlayersList', function(source, cb)
+    local playerList = {}
+
+    for _, playerId in ipairs(GetPlayers()) do
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        local identifier = xPlayer.identifier
+        local discord_id = GetPlayerIdentifier(playerId, 1) or 'Introuvable'
+        local name = GetPlayerName(playerId)
+
+        playerList[#playerList + 1] = {
+            id = playerId,
+            discord_id = discord_id,
+            rpname = xPlayer.getName(),
+            name = namen,
+            identifier = identifier,
+            group = xPlayer.getGroup(),
+            cash = xPlayer.getAccount("money"),
+            bank = xPlayer.getAccount("bank"),
+            coords = xPlayer.getCoords(true),
+            inventory  = xPlayer.getInventory(),
+            job = xPlayer.getJob(), 
+        }
+
+        if #playerList == #GetPlayers() then
+            cb(playerList)
+        end
+    end
+end)
+
+ESX.RegisterServerCallback('admin:checkPermissions', function(source, cb)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.getGroup() ~= 'user' then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
---
+++ Shared/configs.lua
@@ -0,0 +1,29 @@
+--[[
+Copyright © 2024 Atoshi
+
+All rights reserved.
+
+This FiveM base, "Outlaws" and all of its associated files, code, and resources are protected by copyright law. Unauthorized reproduction, distribution, or modification of this base, in whole or in part, without the express permission of the copyright holder, is strictly prohibited.
+
+For licensing inquiries or permission requests, please contact:
+
+https://discord.gg/fivedev
+
+Thank you for respecting our intellectual property rights.
+]]
+
+C = {} 
+
+local OpenMenuKey <const> = 'F9'
+local NoclipCommand <const> = 'noclip'
+local NoclipKey <const> = 'F10'
+local MenuBanner <const> = "https://cdn.discordapp.com/attachments/1279146330337312800/1282850505134968902/Hover.png?ex=66e0db22&is=66df89a2&hm=a6cc6fa3e1d85e617e731c7b1629eb0db6f56ed5a4080a3c27f4bc5d25301235&"
+local Logs <const> = {
+    Staff = 'https://discord.com/api/webhooks/1283215053734281227/c91BS_PfC-u52Vop_vmP2-WalQegWDQK2plzSz8QVwKfJgni7aj6Ik5kQfNI2SsAlunF'
+}
+
+C.OpenMenuKey = OpenMenuKey
+C.NoclipCommand = NoclipCommand
+C.NoclipKey = NoclipKey
+C.MenuBanner = MenuBanner
+C.Logs = Logs
